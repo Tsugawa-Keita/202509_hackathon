fb1ff0c1a8ca37b0e2cc2df0d5af92f6c3070002
Tsugawa-Keita <k-tsugawa@dirisl.com>
2025-09-27 14:09:53 +0900

    chore: update dependencies and integrate Tailwind CSS
    - Added Tailwind CSS and its Vite plugin to dependencies.
    - Updated App component to improve accessibility by adding rel="noopener" to external links.
    - Imported Tailwind CSS in index.css for styling.
    - Configured Vite to use Tailwind CSS plugin.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import reactLogo from "./assets/react.svg";
+
 import "./App.css";
 
 function App() {
@@ -9,11 +10,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://vite.dev" rel="noopener" target="_blank">
+          <img alt="Vite logo" className="logo" src={viteLogo} />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" rel="noopener" target="_blank">
+          <img alt="React logo" className="logo react" src={reactLogo} />
         </a>
       </div>
       <h1>Vite + React</h1>
